294689c8ae4cff2e184b86571fe8dd71fcc0392d
Chaitanya Gandrothu <kgandrot@qti.qualcomm.com>
2026-01-09 03:40:01 -0800

    A17 cp2a upgrade
    Change-Id: I11947214749d4ebc2df0f7b54c75d1f0347141e2
    Signed-off-by: chaitanya gandrothu <kgandrot@qti.qualcomm.com>

--- qti-can-board.mk
+++ qti-can-board.mk
@@ -1,6 +1,6 @@
 # Build QTI-CAN kernel driver
 ifeq ($(TARGET_BOARD_AUTO),true)
-ifneq ($(filter $(PLATFORM_VERSION), 16 Baklava),$(PLATFORM_VERSION))
+ifneq ($(filter $(PLATFORM_VERSION), 16 Baklava CinnamonBun 17),$(PLATFORM_VERSION))
 	BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qti-can.ko
 endif
 endif
